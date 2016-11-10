00c3d72586683de75849edb017bfbbfd23ded31b
A.M. Masudul Haque <saadixl@outlook.com>
2016-11-10 12:41:17 +0600

    Fix

--- xscroll.js
+++ xscroll.js
@@ -1,16 +1,16 @@
 (function($) {
 	$.fn.extend({
-	xScroll: function(options) {
+	xScroll: function(distance, delay) {
 		return this.each(function() {
 				var self = $(this);
 				$(document).keyup(function(e) {
 					switch(e.which) {
 						case 37:
-							leftClick(self);
+							leftClick(self, distance, delay);
 							break;
 
 						case 39:
-							rightClick(self);
+							rightClick(self, distance, delay);
 							break;
 
 						default: return;
@@ -26,7 +26,7 @@
 			});
 		}
 	});
-	function leftClick(self) {
+	function leftClick(self, distance, delay) {
 		var left = self.css('left');
 		if(left === "auto") {
 			left = 0;
@@ -34,27 +34,27 @@
 		var parentWidth = parseInt(self.parent().css('width'));
 		var childWidth = parseInt(self.css('width'));
 		var childLeft = parseInt(self.css('left'));
-		if((childWidth + childLeft) < parentWidth + 100) {
+		if((childWidth + childLeft) < parentWidth + distance) {
 			return;
 		}
 		self.css('position', 'relative');
 		self.animate({
-			left: (parseInt(left) - 100)
-		}, 200);
+			left: (parseInt(left) - distance)
+		}, delay);
 	}
 
-	function rightClick(self) {
+	function rightClick(self, distance, delay) {
 		var left = self.css('left');
-		var currentLeft = parseInt(left) + 100;
+		var currentLeft = parseInt(left) + distance;
 		if(left === "auto") {
 			currentLeft = 0;
 		}
-		if(currentLeft >= -100) {
+		if(currentLeft >= 0) {
 			currentLeft = 0;
 		}
 		self.css('position', 'relative');
 		self.animate({
 			left: currentLeft
-		}, 500);
+		}, delay);
 	}
 })(jQuery);
